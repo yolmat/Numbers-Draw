d96b454c038f3dc51a718935ffc4ce87958b3432
unknown <mateus.saraiva09@gmail.com>
2023-03-22 18:00:39 -0300

    luckNumber

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('.main')
-const li = document.querySelector('ul')
+const ul = document.querySelector('ul')
+const li = document.querySelector('li')
 const firstLi = document.querySelector('#firstItem')
 const input = document.querySelector('#addNumber')
 const btn = document.querySelector('#btn')
@@ -14,25 +15,35 @@ function getNumberArray() {
         return
     }
 
+    setTimeout(function () {
+        firstLi.parentNode.removeChild(firstLi)
+    }, 900)
+
+    const numberOfLuck = luckNumber()
+
+    console.log(numberOfLuck)
+
     while (array.length < number) {
         array.push(array.length + 1)
 
+
         let newLi = document.createElement('li')
 
-        setTimeout(function () { li.appendChild(newLi) }, 900)
+        setTimeout(function () { ul.appendChild(newLi) }, 900)
 
-        setTimeout(function () {
-            firstLi.parentNode.removeChild(firstLi)
-        }, 900)
 
         newLi.innerHTML = array.length
-
     }
 
     btn.disabled = true
 }
 
 btn.addEventListener('click', getNumberArray)
+
+function luckNumber() {
+    let number = Math.floor(Math.random() * input.value) + 1
+    return number
+}
 
 function reloadPage() {
     window.location.reload()
